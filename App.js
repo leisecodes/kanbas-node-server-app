595f8af28f6f80f69aeebf98d180c53e92c21076
lcran <leise.crandall@jax.org>
2024-06-16 13:26:41 -0400

    commit of A6

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import "dotenv/config";
 import express from 'express';
+import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import cors from "cors";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
