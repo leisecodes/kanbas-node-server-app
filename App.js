5604b880b309c4a70925462a8fce6ca4b27e495a
lcran <leise.crandall@jax.org>
2024-06-16 15:24:39 -0400

    commit of A6

--- App.js
+++ App.js
@@ -7,11 +7,32 @@ import Lab5 from './Lab5/index.js';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import cors from "cors";
+import session from "express-session";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000", 
+}));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+  app.use(
+    session(sessionOptions)
+  );
+  
 app.use(express.json());
 UserRoutes(app);
 AssignmentRoutes(app);
